af1f7269b8fd73597b2cc1cef12337f59d9bdd26
Anum Amin <developer.anum@gmail.com>
2023-10-31 23:58:58 +0500

    Change URLs to live server

--- shared/src/commonMain/kotlin/network/Constants.kt
+++ shared/src/commonMain/kotlin/network/Constants.kt
@@ -1,8 +1,8 @@
 package network
 
 object Constants {
-    const val BASE_URL = "http://192.168.1.103:3000/api/v1/"
-    const val SOCKET_URL = "ws://192.168.1.103:3000"
+    const val BASE_URL = "https://draw-guess-0eee4f6ec0e8.herokuapp.com/api/v1/"
+    const val SOCKET_URL = "wss://draw-guess-0eee4f6ec0e8.herokuapp.com"
     const val SOCKET_RECONNECTION = 5000L
     const val RETRY_LIMIT = 3
     const val ROOM_ID = "room_id"
